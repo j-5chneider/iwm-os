e8ee2cd2c1158c39bb5350ccc137ad3908e38370
j-5chneider <juergentschneider@gmx.de>
2019-10-24 11:14:09 +0000

    script to set up MyBinder

---
+++ holepunch_binder_create.R
@@ -0,0 +1,8 @@
+library(holepunch)
+write_compendium_description(package = "iwm OS workshop", description = "files of workshop")
+
+write_dockerfile(maintainer = "Juergen Schneider")
+
+generate_badge()
+
+build_binder()
